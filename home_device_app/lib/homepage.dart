b910dd21642e5538d32f1d685b9536bc2780959c
Pawan Bhayde <pawanbhayde721@gmail.com>
2024-02-12 22:57:12 +0530

    home device app done

--- home_device_app/lib/homepage.dart
+++ home_device_app/lib/homepage.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:glass/glass.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  static final title = 'salomon_bottom_bar';
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,132 +41,247 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: 200,
-              child: Text("Best Management for your home",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  )),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 60,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              const SizedBox(
+                width: 200,
+                child: Text("Best Management for your home",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    const SizedBox(width: 20),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color(0xff6140E5),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Outside',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color(0xff212224),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Living Room',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color(0xff212224),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Bed Room',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  SizedBox(width: 20),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xff6140E5),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Outside',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xffffffff),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                  Product(
+                    title: 'Smart \nSpeaker',
+                    image: 'assets/item2.png',
                   ),
-                  SizedBox(width: 8),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xff212224),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Living Room',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xffffffff),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                  Product(
+                    title: 'Vacuum \nCleaner',
+                    image: 'assets/item1.png',
                   ),
-                  SizedBox(width: 8),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xff212224),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Bed Room',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xffffffff),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 8),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 150,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(
-                      image: AssetImage('assets/item2.png'),
-                      fit: BoxFit.cover,
-                    ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Product(
+                    title: 'Smart \nSpeaker',
+                    image: 'assets/item2.png',
                   ),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Living Room',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
+                  Product(
+                    title: 'Vacuum \nCleaner',
+                    image: 'assets/item1.png',
                   ),
-                ),
-                Container(
-                  width: 150,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(
-                      image: AssetImage('assets/item1.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          /// Home
+          SalomonBottomBarItem(
+            icon: const Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            title: const Text("Home"),
+            selectedColor: Color(0xffFFFFFF),
+          ),
+
+          /// Likes
+          SalomonBottomBarItem(
+            icon: const Icon(
+              Icons.favorite_border,
+              color: Colors.white,
+            ),
+            title: const Text("Likes"),
+            selectedColor: Colors.pink,
+          ),
+
+          /// Search
+          SalomonBottomBarItem(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            title: const Text("Search"),
+            selectedColor: Colors.orange,
+          ),
+
+          /// Profile
+          SalomonBottomBarItem(
+            icon: const Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            title: const Text("Profile"),
+            selectedColor: Colors.teal,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Product extends StatelessWidget {
+  const Product({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+  final String title;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 170,
+      height: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const Spacer(),
+          // on off button
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    side: const BorderSide(
+                      color: Color(0xff1CD570),
+                      width: 2,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.power_settings_new,
+                    color: Color(0xff1CD570),
+                  ),
+                ),
+              ).asGlass(
+                tintColor: Colors.black.withOpacity(0.2),
+                clipBorderRadius: BorderRadius.circular(50),
+              ),
+              const SizedBox(
+                width: 18,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          )
+        ],
       ),
     );
   }
